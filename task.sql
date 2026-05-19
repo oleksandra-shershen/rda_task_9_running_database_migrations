2df07761e14648974aef2ec53b1a49c06c73efcf
Shershen Oleksandra <o_shershen@viyar.ua>
2026-05-19 21:10:11 +0300

    fix(task.sql): correct logic

--- task.sql
+++ task.sql
@@ -39,16 +39,16 @@ CREATE TABLE ProductInventory (
 );
 --rollback DROP TABLE ProductInventory;
 
---changeset mate.academy:5 labels:0.0.2
+--changeset mate.acamemy:5 labels:0.0.2
 CREATE TABLE Users (
     ID INT AUTO_INCREMENT,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
-    Email VARCHAR(50),
+    Email VARCHAR(100),
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE Users;
 
---changeset mate.academy:6 labels:0.0.3
-CREATE index Email on Users (Email);
---rollback DROP INDEX Email on Users;
+--changeset mate.acamemy:6 labels:0.0.3
+CREATE INDEX Email ON Users (Email);
+--rollback DROP INDEX Email ON Users;
